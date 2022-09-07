86ac2971840daac6960e024e4e6eafabcc7e36d8
Ahmed Faarih <faarih@aasandha.mv>
2022-09-07 10:08:20 +0500

    Dashboard and layout files added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Document</title>
-</head>
-<body>
-<div class="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
+<!-- //extending master -->
+@extends('master')
+
+<!-- //add page title here -->
+@section('title', 'Home page')
+
+<!-- //add content of each page here -->
+@section('content')
+
+<div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
+  
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary">Button</button>
+
 </div>
-</body>
-</html>
+
+@stop
